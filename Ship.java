afba80eeae62279e5ff6e309a3dc603f0a8fbb3e
Warston <gohaun15@gmail.com>
2026-02-24 00:11:37 -0500

    added repository

---
+++ Ship.java
@@ -0,0 +1,12 @@
+class Ship{
+	int length = 0;
+	String name = "";
+	int position[] = {};
+	boolean horizontal = false;
+
+	Ship(String newName, int newLength){
+		name = newName;
+		length = newLength;
+	}
+
+}
